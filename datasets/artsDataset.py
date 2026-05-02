b1f95533bcf5038376c96d568bccd98be300f34c
EC2 Default User <ec2-user@ip-172-31-26-142.ec2.internal>
2026-05-02 14:38:56 +0000

    able to load up dataset and cleaned the script up a little bit

--- datasets/artsDataset.py
+++ datasets/artsDataset.py
@@ -13,24 +13,39 @@ class artsDataset(baseTrafficSignDataset):
     Docstring for artsDataset
     
     __init__(self, root, difficulty="easy"): Initializes the dataset with the root directory and difficulty level.
-        root: str - The root directory of the dataset.
-        difficulty: str - The difficulty level of the dataset options are: "easy","challenging"
+        root (str) - The root directory of the dataset.
+        
+        difficulty (str) - The difficulty level of the dataset options are: "easy","challenging"
     
-        load_images(self): Method to load images from the dataset
+        images_sub_dir (str) - TODO: Fill this in 
+        
+        annotations_subdir (str) - TODO: Fill this in
+        
+    load_images(self): Method to load images from the dataset
     
     '''
+    
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% __init__
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def __init__(self, root,difficulty="easy", images_subdir="JPEGImages", annotations_subdir="Annotations"):
         self.difficulty = difficulty
         super().__init__(root)
         
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% load_images_as_numpy
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def load_images_as_numpy(self, image_files):
-        images = []
+        images = {}
         for img_file in tqdm(image_files,desc="Loading Images"):
             img = Image.open(img_file)
             img_array = np.array(img)
-            images.append(img_array)
+            images[os.path.basename(img_file[:-4])] = img_array
         return images
     
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% parse_annotation
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def parse_annotation(self, xml_file):
         tree = ET.parse(xml_file)
         root = tree.getroot()
@@ -64,24 +79,51 @@ class artsDataset(baseTrafficSignDataset):
             "objects": objects
         }
         
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% load_images
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def load_images(self, root_dir, images_subdir):
         image_files = glob.glob(os.path.join(root_dir, self.difficulty, images_subdir,"*.jpg"))
-        return self.load_images_as_numpy(image_files)
+        return self.load_images_as_numpy(image_files[:3000]) # TODO: request more memory from aws, g4dn.2xlarge not enough
 
     
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% load_annotations
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def load_annotations(self, root_dir, annotations_subdir):
         annotation_files = glob.glob(os.path.join(root_dir, self.difficulty, annotations_subdir,"*.xml"))
         return [self.parse_annotation(ann_file) for ann_file in tqdm(annotation_files,desc="Loading Annotations")]
-
+    
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% __getitem__
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def __getitem__(self, index):
         # Implementation for getting an item by index specific to artsDataset
         pass    
 
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #% __len__
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     def __len__(self):
         # Implementation for getting the length of the dataset specific to artsDataset
         pass
     
 
 if __name__ == "__main__":
-    dataset = artsDataset("/data/arts", difficulty="easy", images_subdir="JPEGImages", annotations_subdir="Annotations")
+    dataset = artsDataset("/mnt/data/arts", difficulty="easy", images_subdir="JPEGImages", annotations_subdir="Annotations")
+    
+    # validate that annotations are sequential with images, currently can only load 3000 images from arts dataset
+    
+    # Generate visualizations
+
+    
+    # come up with models
+    
+    # determine metrics
+    
+    # perform classification
+    
+    # scale up
+    
+    # augmentation
     print(f"Dataset length: {len(dataset)}")
