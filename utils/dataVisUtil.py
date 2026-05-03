d17ce083472eef994a31f74816879bebb1d7bbe4
EC2 Default User <ec2-user@ip-172-31-26-142.ec2.internal>
2026-05-03 16:21:25 +0000

    added commits to dataVisUtil script

--- utils/dataVisUtil.py
+++ utils/dataVisUtil.py
@@ -2,23 +2,29 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 def dataVis(image,annotation:dict):
+    # determine the id for the current data sample
     id = annotation['id']
     
+    # Plot the image
     fig,ax = plt.subplots()
     ax.imshow(image)
     
+    # Loop through each label, determine where the object is located, and plot a rectangle around it
     for obj in annotation['objects']:
+        # determine where upper left (UL) and lower right (LR) corners of the bounding box are located
         bbox = obj['bbox']
-        upper_left = bbox[:2]
-        lower_right = bbox[2:]
+        ul = bbox[:2]
+        lr = bbox[2:]
         
-        width = lower_right[0] - upper_left[0]
-        height = lower_right[1] - upper_left[1]
+        width = lr[0] - ul[0]
+        height = lr[1] - ul[1]
         
-        rect = patches.Rectangle(upper_left,width,height,linewidth=1,edgecolor='r',alpha=0.3)
+        # generate rect around the object within the image
+        rect = patches.Rectangle(ul,width,height,linewidth=1,edgecolor='r',alpha=0.3)
         ax.add_patch(rect)
         
-        ax.text(upper_left[0]-30,upper_left[1]-30, obj['label'],fontsize=6,fontweight='bold',bbox=dict(facecolor='lime', alpha=0.3, edgecolor='none'))
+        # add the label of the object to the image
+        ax.text(ul[0]-30,ul[1]-30, obj['label'],fontsize=6,fontweight='bold',bbox=dict(facecolor='lime', alpha=0.3, edgecolor='none'))
     
-    
+    # save the figure TODO: Determine if I should use absolute paths or not
     plt.savefig(fr'EDOTS/figures/labeled_{id}.png')
